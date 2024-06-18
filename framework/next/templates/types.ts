653fa5e1a21395eb5894ebc9bdac8159cf3bcafb
nakayan5 <tomoya.progress@gmail.com>
2024-06-19 01:08:24 +0900

    feat: add app template for next ts

---
+++ framework/next/templates/types.ts
@@ -0,0 +1,20 @@
+import { PackageManager } from '../../../helpers';
+
+// add it when create template
+export type TemplateType = 'app-empty' | 'app' | 'default' | 'default-empty';
+
+export type TemplateMode = 'js' | 'ts';
+
+export interface InstallTemplateArgs {
+  appName: string;
+  root: string;
+  packageManager: PackageManager;
+  isOnline: boolean;
+  template: TemplateType;
+  mode: TemplateMode;
+  eslint: boolean;
+  srcDir: boolean;
+  importAlias: string;
+  skipInstall: boolean;
+  turbo: boolean;
+}
