2b851cd0db86f40c30764344692ffafaadfacb2f
koralle <mkhd7930@gmail.com>
2024-08-06 07:19:25 +0900

    feat: add `isBoolean`

--- helpers/is.ts
+++ helpers/is.ts
@@ -1,2 +1,5 @@
 export const isString = (value: any): value is string =>
   Object.prototype.toString.call(value) === '[object String]';
+
+export const isBoolean = (value: any): value is boolean =>
+  typeof value === 'boolean';
